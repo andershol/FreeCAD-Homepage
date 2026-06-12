51c8a54f50cefd819ed0e770dd881c3dc81ff810
andershol <andershol@users.noreply.github.com>
2026-06-12 19:14:46 +0200

    Fix some accessibility issues

--- translation.php
+++ translation.php
@@ -18,23 +18,21 @@ function loadLocaleMap($filePath)
 
 function getFlags($href = '/')
 {
+    global $locale;
     $localeMap = loadLocaleMap(__DIR__ . '/localeMap.json');
 
     // Default English entry
     echo('<a class="dropdown-item" href="' . $href . '">
             <img src="lang/en/flag.jpg" alt="" />' . _('English') . '</a>');
 
-    foreach ($localeMap as $shortCode => $locale) {
+    foreach ($localeMap as $shortCode => $localeCode) {
         if ($shortCode === 'en') {
             continue;
         }
-        $localeObj = locale_get_display_language($locale, $locale);
-        if (!$localeObj) {
-            $localeObj = $locale; // fallback
-        }
-
-        echo('<a class="dropdown-item" href="' . $href . '?lang=' . $shortCode . '">
-                <img src="lang/' . $shortCode . '/flag.jpg" alt="" />' . _($localeObj) . '</a>');
+        $localeObj = locale_get_display_language($localeCode, $localeCode) ?? $localeCode;
+        echo('<a class="dropdown-item" href="' . $href . '?lang=' . $shortCode . '"'.
+            ($localeCode == $locale ? ' aria-current="page"' : '').'>'.
+            '<img src="lang/' . $shortCode . '/flag.jpg" alt="" />' . _($localeObj) . '</a>');
     }
 }
 
@@ -50,6 +48,7 @@ function getLangParam(): string {
 $lang = getLangParam();
 $localeMap = loadLocaleMap(__DIR__ . '/localeMap.json');
 $locale = isset($localeMap[$lang]) ? $localeMap[$lang] : $lang;
+$localeName = locale_get_display_language($locale, $locale) ?? $locale;
 putenv("LC_ALL=$locale");
 setlocale(LC_ALL, $locale);
 bindtextdomain("homepage", "lang");
